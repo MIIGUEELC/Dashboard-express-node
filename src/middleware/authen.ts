fa2fb728e00538aa40e3ab0263b238d81ddb3567
MIIGUEELC <miigueel9219@gmail.com>
2025-01-08 14:21:25 +0100

    cambios autenticacion mid

--- src/middleware/authen.ts
+++ src/middleware/authen.ts
@@ -1,28 +1,27 @@
-
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
-// Cargar variables de entorno
+
 dotenv.config();
 
-// Obtener la clave secreta desde el archivo .env
+
 const SECRET_KEY = process.env.SECRET_KEY || "default_secret_key";
 
-// Middleware de autenticación
+
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers['authorization']?.split(' ')[1]; // El formato debe ser "Bearer <token>"
+    const token = req.header('Authorization')?.replace('Bearer ', '');
 
-  if (!token) {
-    return res.status(401).json({ error: 'Token no proporcionado' });
-  }
-
-  // Verificar el token con la clave secreta
-  jwt.verify(token, SECRET_KEY, (err, user) => {
-    if (err) {
-      return res.status(403).json({ error: 'Token no válido' });
+    if (!token) {
+        res.status(401).json({ message: 'Acces Denied, No token provided.' });
+        return;
     }
-    req.user = user;
-    next();
-  });
-};
+
+    try {
+        jwt.verify(token, SECRET_KEY);
+        next();
+    } catch (error) {
+        res.status(403).json({ message: 'Invalid token.' })
+        return;
+    }
+}
