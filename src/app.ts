03ad8c3c9f4f939f2663affc0ae8853df05cce4a
MIIGUEELC <miigueel9219@gmail.com>
2025-01-10 12:26:12 +0100

    routes y app

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,41 @@
-import express from 'express';
-import bookingRoutes from './routes/bookingroutes';
+import express from "express";
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
+
+
+// Import auth middleware for private routes
+import { authenticateToken } from './middleware/auth';
+
+// Import routes
+import { loginRoute } from './controllers/login.controller';
+import { bookingRoutes } from './routes/booking.routes'
+import { roomRoutes } from './routes/room.routes';
+import { userRoutes } from './routes/user.routes';
+import { contactRoutes } from './routes/contact.routes';
+import { infoController } from "./controllers/information.controller";
+
+export const app = express();
+
+const port = 3000;
+// Middleware
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Rutas de bookings
-app.use('/api/bookings', bookingRoutes);
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+// Public routes
+app.get('/', (req, res) => {
+    res.send('API running on root path');
 });
+app.use('/info', infoController);
+app.use('/login', loginRoute);
+
+// Private routes with authenticate token
+app.use('/bookings', authenticateToken, bookingRoutes);
+app.use('/rooms', authenticateToken, roomRoutes);
+app.use('/users', authenticateToken, userRoutes);
+app.use('/contacts', authenticateToken, contactRoutes);
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
